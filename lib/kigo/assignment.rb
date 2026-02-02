a5080598d87b59540c7e9ceef1aaf2a236dd983d
Delon R. Newman <contact@delonnewman.name>
2026-02-02 17:07:37 -0500

    Add from_data

--- lib/kigo/assignment.rb
+++ lib/kigo/assignment.rb
@@ -2,6 +2,12 @@ module Kigo
   class Assignment
     attr_reader :subject, :value
 
+    def self.from_data(form)
+      raise SyntaxError, "invalid form, expected: (set! SUBJECT VALUE)" unless form.length == 3
+
+      new(form[1], form[2])
+    end
+
     def initialize(subject, value)
       @subject = subject
       @value   = value
